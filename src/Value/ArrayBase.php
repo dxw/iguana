b3ae950bdaae2deb6d05ed0d60dfe5eb68226cf2
Tom Adams <tom@dxw.com>
2017-09-26 08:32:50 -0400

    fixup! fixup! WIP: Use vimeo/psalm

--- src/Value/ArrayBase.php
+++ src/Value/ArrayBase.php
@@ -4,17 +4,16 @@ namespace Dxw\Iguana\Value;
 
 abstract class ArrayBase implements \ArrayAccess
 {
-    /** @var array */
+    /** @var array|null */
     protected $value;
 
     abstract protected function getDefault(): array;
 
     final public function __construct(array $value=null)
     {
-        if ($value === null) {
+        $this->value = $value;
+        if ($this->value === null) {
             $this->value = $this->getDefault();
-        } else {
-            $this->value = $value;
         }
     }
 
